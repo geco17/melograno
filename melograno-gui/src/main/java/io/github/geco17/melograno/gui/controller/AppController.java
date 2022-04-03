ce16ab73dedd7ab94a9190d99fe80c946dde6151
William Burnham <burnham.william@gmail.com>
2022-04-03 18:27:54 +0200

    context / edit menu

--- melograno-gui/src/main/java/io/github/geco17/melograno/gui/controller/AppController.java
+++ melograno-gui/src/main/java/io/github/geco17/melograno/gui/controller/AppController.java
@@ -3,11 +3,19 @@ package io.github.geco17.melograno.gui.controller;
 import io.github.geco17.melograno.gui.util.S;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
-public class AppController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class AppController implements Initializable {
+
+    @FXML
+    private Menu editMenu;
+    @FXML
+    private ContextMenu editorContextMenu;
     @FXML
     private MenuBar menuBar;
     @FXML
@@ -23,5 +31,32 @@ public class AppController {
 
     public void exitActionHandler(ActionEvent actionEvent) {
         ((Stage) menuBar.getScene().getWindow()).close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // bug or feature? the context menu must be initialized first, then the edit menu can be populated
+        editorContextMenu.getItems()
+                .forEach(item -> editMenu.getItems().add(item));
+    }
+
+    public void editDeleteActionHandler(ActionEvent actionEvent) {
+        textEditor.deleteText(textEditor.getSelection());
+    }
+
+    public void editPasteActionHandler(ActionEvent actionEvent) {
+        textEditor.paste();
+    }
+
+    public void editCopyActionHandler(ActionEvent actionEvent) {
+        textEditor.copy();
+    }
+
+    public void editCutActionHandler(ActionEvent actionEvent) {
+        textEditor.cut();
+    }
+
+    public void editSelectAllActionHandler(ActionEvent actionEvent) {
+        textEditor.selectAll();
     }
 }
